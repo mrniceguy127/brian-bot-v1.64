7c537ad124aa7778601a46cd1e0b4f9ba042f557
Matthew Kleiner <mkleiner2670@gmail.com>
2019-01-16 16:39:41 -0500

    Added code to get started with the Slack API.

---
+++ index.js
@@ -0,0 +1,33 @@
+'use-strict';
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const Botkit = require('botkit');
+
+const slackToken = process.env.CLIENT_TOKEN;
+const slackOAuth = process.env.CLIENT_OAUTH;
+const slackSigningSecret = process.env.CLIENT_SIGNING_SECRET;
+
+const slackController = Botkit.slackbot({
+  require_delivery: true,
+  clientSigningSecret: clientSigningSecret
+});
+
+const slackBot = slackController.spawn({
+  token: slackToken
+});
+
+slackBot.startRTM((err, bot, payload) => {
+  if (err) {
+    throw new Error('Brian could not connect to slack!');
+  } else {
+    slackController.log("Slack connection established!");
+  }
+});
+
+slackController.hears(["mantissa"], ["ambient"], (bot, message) => {
+  bot.startConversation(message, (err, conversation) => {
+    conversation.say("BEWARE!");
+  });
+});
